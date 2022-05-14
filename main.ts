b6a5ab2ceb4399d6e4fc3ccf7811ec60b1289b71
tienpvse1 <tienpvse140613@fpt.edu.vn>
2022-05-14 10:25:52 +0700

    change

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World updated !");
+  res.send("Hello World updatedtufyuitfuit !");
 });
 
 app.listen(3000);
